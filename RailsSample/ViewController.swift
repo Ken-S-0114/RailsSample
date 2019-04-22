97f421eb02c7219e904846d10f03af9a0c79c5d6
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2019-04-22 23:40:29 +0900

    first commit

---
+++ RailsSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RailsSample
+//
+//  Created by 佐藤賢 on 2019/04/22.
+//  Copyright © 2019 佐藤賢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
